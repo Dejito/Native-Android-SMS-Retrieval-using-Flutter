a39691f434cb34d80d5f828d298f6175e32fa643
dejito <Deerealboy@gmail.com>
2026-04-01 23:16:11 +0100

    implemented otp screen ui

--- android/app/src/main/kotlin/com/example/native_android_sms_retrieval_with_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/native_android_sms_retrieval_with_flutter/MainActivity.kt
@@ -1,9 +1,5 @@
 package com.example.native_android_sms_retrieval_with_flutter
 
-import io.flutter.embedding.android.FlutterActivity
-
-//class MainActivity : FlutterActivity()
-
 import android.app.Activity
 import android.content.*
 import android.os.Bundle
